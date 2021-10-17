c1bddba9a80a18b48286e50622312cc77ca92707
Rostom Baccar <81230614+98rostom@users.noreply.github.com>
2021-10-17 21:45:20 +0200

    codeurincremental.h update + file changes

--- _SERVICES/CodeurIncremental.c
+++ _SERVICES/CodeurIncremental.c
@@ -2,8 +2,6 @@
 
 //Fonction qui récupère l'angle donné par le codeur incrémental
 int recup_angle(TIM_TypeDef * timer){
-    //Fonction qui récupère l'angle fournit par l'encodeur
-    //La fonction de l'encodeur prend en paramètre le timer associé à l'encodeur
-    
-    return LL_TIM_GetCounter(timer)/2
 }
+
+//Fonction qui récupère 
