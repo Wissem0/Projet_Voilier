c7be10b3b22db27b6ceb05fd1de2ae24d02f2501
Rostom <rostombaccar.et@gmail.com>
2021-11-04 20:52:31 +0100

    init girouette faite + handler interruption

--- _SERVICES/GestionGirouette.h
+++ _SERVICES/GestionGirouette.h
@@ -2,10 +2,6 @@
 #include "MyGPIO.h"
 #include "MyTimer.h"
 
-//Fonction qui récupère l'angle donné par le codeur incrémental
-int Girouette_recup_angle(TIM_TypeDef * timer);
-
-//Fonction qui récupère la direction du mouvement de la girouette
-int Girouette_recup_direction(TIM_TypeDef * timer);
-
+void Girouette_recup_angle();
 void Girouette_init_girouette(MyTimer_Struct_TypeDef * timer_encodeur,GPIO_TypeDef * GPIO);
+
