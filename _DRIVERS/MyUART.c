e1e484a700470a494f45802a3c0244e712daa863
AlaeNourdine <nourdine@etud.insa-toulouse.fr>
2021-11-05 13:02:52 +0100

    UART

--- _DRIVERS/MyUART.c
+++ _DRIVERS/MyUART.c
@@ -1,20 +1,11 @@
 #include "MyUART.h"
+#include "MyGPIO.h"
 
 
-
-void MyUART_Init ( MyUART_Struct_TypeDef * UART ) 
-{
-	
+//Envoie le message 
+void MyUART_Send (char String) {
+	USART1->DR |= String; // Ecriture de la donnée dans le registre DR
+	while(!(USART1->SR & USART_SR_TC)) {} // Attente de la fin de transmission
 }
 
-void MyADC_Start( MyADC_Struct_TypeDef * ADC ) 
-{
 
-}
-
-float MyADC_Read ( MyADC_Struct_TypeDef * ADC);
-{
-
-}
-
-}
--- _DRIVERS/MyUART.h
+++ _DRIVERS/MyUART.h
@@ -2,18 +2,13 @@
 #define MYUART_H
 #include "stm32f10x.h"
 
-typedef struct
-{
-	USART_TypeDef * UART ;
-	int UART_BPS ; 
-
-} MyUART_Struct_TypeDef ;
 
 
-//Configure l'UART
-void MyUART_Init ( MyUART_Struct_TypeDef * UART ) ;
 //Envoie le message 
-void MyUART_Send ( USART_TypeDef * UART, char* String);
+void MyUART_Send (char String);
+
+//Reçoit le message
+//void MyUART_Rec ( USART_TypeDef * UART, char* String);
 
 
 
