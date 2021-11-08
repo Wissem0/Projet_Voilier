ed67e83f1bfe7cc9a8e5b24bcd5de0e75e16d48b
AlaeNourdine <nourdine@etud.insa-toulouse.fr>
2021-11-08 15:38:10 +0100

    initial commit Alae

--- _SERVICES/Orientation.c
+++ _SERVICES/Orientation.c
@@ -1,23 +1,32 @@
-#include "Driver_GPIO.h"
-#include "Driver_Timer.h"
-
+#include "MyGPIO.h"
+#include "MyTimer.h"
+#include "stm32f10x.h"
 #include "Orientation.h"
 
-void Orientation_Init(void){
-	// PWM (PB6)
-	GPIO_Init(GPIOB, 6, AltOut_Ppull_2MHZ);
-	// Bit de sens (PC7)
-	GPIO_Init(GPIOC, 7, Out_Ppull_2MHZ);
+
+MyGPIO_Struct_TypeDef sortiePWM = {GPIOA, 1, AltOut_Ppull} ;
+MyGPIO_Struct_TypeDef sortieBitSens = {GPIOC, 7, Out_Ppull} ;
+MyTimer_Struct_TypeDef MonTimer;
 	
-	// Initialisation du Timer 4 (20kHz)
-	Timer_Base_Init(TIM4, 3599, 0);
+void Orientation_Init(void){
+	// PWM (PA1)
+	MyGPIO_Init(&sortiePWM);
+	// Bit de sens (PC7)
+	MyGPIO_Init(&sortieBitSens);
+	
+	// Initialisation du Timer 2 pour une fréquence 20kHz)
+	MonTimer.Timer = TIM2 ;
+	MonTimer.ARR = 3599 ;
+	MonTimer.PSC = 0 ;
+	MyTimer_Base_Init(&MonTimer);
 	
 	// Initialisation de la PWM sur le channel 1 du Timer 4
-	Timer_PWM(TIM4, 1);
+	MyTimer_PWM(TIM4, 1);
 	
 	// Etablissement du rapport cyclique à 0%
-	Timer_Set_Cycle_PWM(TIM4, 1, 0);
+	MyTimer_PWM_Cycle(TIM4, 1, 0);
 	
 	// Lancement du compteur
-	Timer_Base_Start(TIM4);
+	MyTimer_Base_Start(TIM4);
 }
+
